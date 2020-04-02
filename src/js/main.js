74f3468394f8326c77950cf14245d4c394093408
Aleksandr <aleksandrvorzhevua@gmail.com>
2020-04-02 22:02:03 +0300

    added all pages.
    added all pages.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
   $(".slider__wraper").owlCarousel({
     items: 2,
@@ -16,3 +17,31 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $("#conference").select2();
+});
+
+$(document).ready(function() {
+  $(".conference").select2();
+});
+
+$(".partners__slider").owlCarousel({
+  loop: true,
+  margin: 12,
+  items: 6,
+  dots: false,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 4
+    },
+    1000: {
+      items: 6
+    }
+  }
+});
+
+
