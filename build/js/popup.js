7bcf74f59e88c80c66c2aedf08660be3ef26a88a
Aleksandr <aleksandrvorzhevua@gmail.com>
2020-04-05 13:27:05 +0300

    update

--- build/js/popup.js
+++ build/js/popup.js
@@ -1,5 +1,8 @@
   // Modal
 
+  let WidthWithOutScroll = document.body.scrollWidth - document.body.clientWidth;
+  console.log(WidthWithOutScroll);
+
   function modalWindow(button, close, modal) { 
     let more = document.querySelector(button), // Получаем аргументы.
       closeWindow = document.querySelector(close), // Получаем аргументы.
@@ -9,13 +12,17 @@
       overlay.style.display = "block"; // Меняем стиль попапа на блок.
       this.classList.add("more-splash"); // Добавляем кнопке эффект через класс more-splash.
       document.body.style.overflow = "hidden"; // Добавляем overflow hidden, что бы не было прокрутки когда активный попап.
+      document.body.style.paddingRight = WidthWithOutScroll + 4 + "px"; // Добавляем overflow hidden, что бы не было прокрутки когда активный попап.
+
     });
 
     closeWindow.addEventListener("click", function() { // при клике на closeWindow.
       overlay.style.display = "none"; // Закрываем modal.
       more.classList.remove("more-splash"); // И убираем у кнопки класс с анимацией.
       document.body.style.overflow = ""; // Убираем overflow.
+      document.body.style.paddingRight = 0 + "px"; // Добавляем overflow hidden, что бы не было прокрутки когда активный попап.
     });
   }
 
   modalWindow(".menu__second-button", ".popup-close", ".overlay");
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,83 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
   $(".slider__wraper").owlCarousel({
     items: 2,
     loop: true,
     dots: false,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       600: {
-        items: 3
+        items: 1,
       },
       1000: {
-        items: 2
-      }
-    }
+        items: 1,
+      },
+    },
   });
+
+  function menuDrop(click, dropList) {
+    $(click).on("click", function (e) {
+      e.preventDefault();
+      $(dropList).slideToggle(200);
+      $(document).mouseup(function (e) {
+        // событие клика по веб-документу
+        let div = $(click); // тут указываем ID элемента
+        if (
+          !div.is(e.target) && // если клик был не по нашему блоку
+          div.has(e.target).length === 0
+        ) {
+          $(dropList).hide(100); // скрываем его
+        }
+      });
+    });
+  }
+
+  menuDrop(".menu__item-exhibitors", ".menu__item-exhibitors-list");
+  menuDrop(".menu__item-speakers", ".menu__item-speaker-list");
+  menuDrop(".menu__item-sponsors", ".menu__item-sponsors-list");
+
+  $(".burger-menu").on("click", function (e) {
+    e.preventDefault();
+    $(".menu__list").toggleClass("menu__list--active");
+    $(".burger-menu").fadeToggle();
+
+    $(document).mouseup(function (e) {
+      let div = $(".menu__item");
+      if (
+        !div.is(e.target) && // если клик был не по нашему блоку
+        div.has(e.target).length === 0
+      ) {
+        $(".menu__list").removeClass("menu__list--active");
+        $(".burger-menu").fadeToggle();
+      }
+    });
+  });
+
+  // $(".burger-menu").on("click", function(e) {
+  //   e.preventDefault();
+  //   $(".menu__list").toggleClass("menu__list--active");
+
+  //   $(document).mouseup(function(e) {
+  //     let div = $(".menu__list");
+  //     if (!div.is(e.target) && div.has(e.target).length === 0) {
+  //       $(".menu__list").removeClass("menu__list--active");
+  //     }
+  //   });
+  // });
+
+  // $(".burger-menu").on("click", function (e) {
+  //   e.preventDefault();
+  //   $(".menu__list").toggleClass("menu__list--active");
+
+  // });
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $("#conference").select2();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".conference").select2();
 });
 
@@ -31,17 +86,30 @@ $(".partners__slider").owlCarousel({
   margin: 12,
   items: 6,
   dots: false,
+  responsiveClass: true,
   responsive: {
-    0: {
-      items: 1
+    400: {
+      items: 2,
+      autoWidth: true,
     },
-    600: {
-      items: 4
+    700: {
+      items: 2,
+      autoWidth: true,
+    },
+    800: {
+      items: 3,
+      autoWidth: true,
+    },
+    900: {
+      items: 3,
+      autoWidth: true,
     },
     1000: {
-      items: 6
-    }
-  }
+      items: 4,
+      autoWidth: true,
+    },
+    1199: {
+      items: 4,
+    },
+  },
 });
-
-
